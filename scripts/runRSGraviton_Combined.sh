620ad67e1dabe27a0bff47adc58aeefcf85c77ab
Ine Arts <ine.arts@telenet.be>
2017-09-08 23:41:24 +0200

    modified:   config_FCC/RSGraviton_ww/fcc_v01/Combined/RSG_ww_10TeV.config 	modified:   config_FCC/RSGraviton_ww/fcc_v01/Combined/RSG_ww_15TeV.config

--- scripts/runRSGraviton_Combined.sh
+++ scripts/runRSGraviton_Combined.sh
@@ -1,5 +1,5 @@
-for ene in 2 5 10 15 20 25 30 35 40;
+for ene in 5 10 15 20 25 30 35 40;
 do
-    ./myFit.exe mwfl config_FCC/RSGraviton_ww/Combined/RSG_ww_"$ene"TeV.config
+    ./myFit.exe mwfl config_FCC/RSGraviton_ww/fcc_v01/Combined/RSG_ww_"$ene"TeV.config
 done
 
--- scripts/runRSGraviton_allHad_fcc_v01.sh
+++ scripts/runRSGraviton_allHad_fcc_v01.sh
@@ -1,12 +1,12 @@
-for ene in 2 5 10 15 20 25 30 35 40;
+for ene in 5 10 15 20 25 30 35 40;
 do
 
-    ./myFit.exe h config_FCC/RSGraviton_ww/allHad/RSG_allHad_"$ene"TeV.config
-    ./myFit.exe w config_FCC/RSGraviton_ww/allHad/RSG_allHad_"$ene"TeV.config
-    ./myFit.exe f config_FCC/RSGraviton_ww/allHad/RSG_allHad_"$ene"TeV.config
-    ./myFit.exe d config_FCC/RSGraviton_ww/allHad/RSG_allHad_"$ene"TeV.config
-    ./myFit.exe p config_FCC/RSGraviton_ww/allHad/RSG_allHad_"$ene"TeV.config
-    ./myFit.exe l config_FCC/RSGraviton_ww/allHad/RSG_allHad_"$ene"TeV.config
+    ./myFit.exe h config_FCC/RSGraviton_ww/fcc_v01/allHad/RSG_allHad_"$ene"TeV.config
+    ./myFit.exe w config_FCC/RSGraviton_ww/fcc_v01/allHad/RSG_allHad_"$ene"TeV.config
+    ./myFit.exe f config_FCC/RSGraviton_ww/fcc_v01/allHad/RSG_allHad_"$ene"TeV.config
+    ./myFit.exe d config_FCC/RSGraviton_ww/fcc_v01/allHad/RSG_allHad_"$ene"TeV.config
+    ./myFit.exe p config_FCC/RSGraviton_ww/fcc_v01/allHad/RSG_allHad_"$ene"TeV.config
+    ./myFit.exe l config_FCC/RSGraviton_ww/fcc_v01/allHad/RSG_allHad_"$ene"TeV.config
 
 done
 
