21141ee392709a3d0045c3c74956d2247ca268e3
David Olivier Jamin <david.jamin@cern.ch>
2018-05-30 11:17:12 +0200

    latest configs from David

--- scripts/RSG_ww/run_signiplot.sh
+++ scripts/RSG_ww/run_signiplot.sh
@@ -1,2 +1,2 @@
-python python/significance_plot.py -f "Data/bkup/RSG_ww_fcc_v02_cut.json" -n "ww" -p ww_cut
-python python/significance_plot.py -f "Data/bkup/RSG_ww_fcc_v02_tagger.json" -n "ww" -p ww_tagger
+python python/significance_plot.py -f "Data/RSG_ww_fcc_v02_cut.json" -n "ww" -p ww_cut
+python python/significance_plot.py -f "Data/RSG_ww_fcc_v02_tagger.json" -n "ww" -p ww_tagger
--- scripts/Zprime_ll/run_signiplot.sh
+++ scripts/Zprime_ll/run_signiplot.sh
@@ -2,4 +2,5 @@ python python/significance_plot.py -f "Data/Zprime_ee_fcc_v02.json" -n "ee" -p e
 python python/significance_plot.py -f "Data/Zprime_mumu_fcc_v02.json" -n "mumu" -p mumu
 python python/significance_plot.py -f "Data/Zprime_ll_fcc_v02.json" -n "ll" -p ll
 python python/significance_plot.py -f "Data/Zprime_ee_fcc_v02.json Data/Zprime_mumu_fcc_v02.json Data/Zprime_ll_fcc_v02.json" -n "ee mumu ll" -p ll_comb
-python python/significance_plot.py -f "Data/bkup/Zprime_mumu_flav_ano_fcc_v02.json" -n "mumu_flav_ano" -p mumu
+python python/significance_plot.py -f "Data/May2018_highstatprod/FCChh/Zprime_mumu_flav_ano_fcc_v02.json" -n "mumu_flav_ano" -p mumu
+python python/significance_plot.py -f "Data/May2018_highstatprod/FCChh/LQ_mumu_fcc_v02.json" -n "mumu_LQ" -p mumu
