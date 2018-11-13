2d20d5b46b8b391614a8db344b508a7f378e85d5
David Olivier Jamin <david.jamin@cern.ch>
2018-11-13 13:37:31 +0100

    update scripts/HELHC/Zprime_ll/

--- scripts/HELHC/Zprime_ll/run_signi.sh
+++ scripts/HELHC/Zprime_ll/run_signi.sh
@@ -2,4 +2,8 @@ python python/significance.py -f "config_FCC/Zprime_ll/helhc_v01/Zprime_ee_*TeV.
 python python/significance.py -f "config_FCC/Zprime_ll/helhc_v01/Zprime_mumu_*TeV.config" -n Zprime_mumu_helhc_v01
 python python/significance.py -f "config_FCC/Zprime_ll/helhc_v01/Zprime_ll_*TeV.config" -n Zprime_ll_helhc_v01 -c
 #python python/significance.py -f "config_FCC/Zprime_mumu_ano/helhc_v01/Zprime_mumu_*TeV.config" -n Zprime_mumu_flav_ano_fcc_v02
+#
+python python/significance.py -f "config_HELHC/Zprime_models/helhc_v01/tagger_SSM_TRFbtag/tt/Zprime_sel0_*TeV.config" -n Zprime_tt_helhc_v01
+python python/significance.py -f "config_HELHC/Zprime_models/helhc_v01/tagger_SSM_TRFbtag/bb/Zprime_sel0_*TeV.config" -n Zprime_bb_helhc_v01
+python python/significance.py -f "config_HELHC/Zprime_models/helhc_v01/tagger_SSM_TRFbtag/qq/Zprime_sel0_*TeV.config" -n Zprime_qq_helhc_v01
 
--- scripts/HELHC/Zprime_ll/run_signiplot.sh
+++ scripts/HELHC/Zprime_ll/run_signiplot.sh
@@ -3,3 +3,8 @@ python python/significance_plot.py -f "Data/Zprime_mumu_helhc_v01.json" -n "mumu
 python python/significance_plot.py -f "Data/Zprime_ll_helhc_v01.json" -n "ll" -p ll --helhc
 python python/significance_plot.py -f "Data/Zprime_ee_helhc_v01.json Data/Zprime_mumu_helhc_v01.json Data/Zprime_ll_helhc_v01.json" -n "ee mumu ll" -p ll_comb --helhc
 #python python/significance_plot.py -f "Data/bkup/Zprime_mumu_flav_ano_fcc_v02.json" -n "mumu_flav_ano" -p mumu
+#
+python python/significance_plot.py -f "Data/Zprime_tt_helhc_v01.json" -n "tt" -p tt --helhc
+python python/significance_plot.py -f "Data/Zprime_bb_helhc_v01.json" -n "bb" -p bb --helhc
+python python/significance_plot.py -f "Data/Zprime_qq_helhc_v01.json" -n "qq" -p qq --helhc
+
